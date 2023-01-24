279b080d0608c91766fa5840e4a2354240f73c53
Rafael Malta <rafa_rsbr@hotmail.com>
2023-01-23 21:15:49 -0300

    Guid e Classe Abstrata

--- ProjetoBaltaPOO/ConteudoContexto/Conteudo.cs
+++ ProjetoBaltaPOO/ConteudoContexto/Conteudo.cs
@@ -1,9 +1,14 @@
 ﻿namespace ProjetoBaltaPOO.ConteudoContexto
 {
-    public class Conteudo
+    public abstract class Conteudo
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Titulo { get; set; }
         public string Url { get; set; }
+
+        public Conteudo()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
