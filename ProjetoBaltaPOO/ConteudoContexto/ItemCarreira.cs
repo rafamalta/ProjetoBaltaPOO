28e631faa1b2daa2007427557648226cff4c3d37
Rafael Malta <rafa_rsbr@hotmail.com>
2023-01-29 12:25:44 -0300

    Construtor e Listas

--- ProjetoBaltaPOO/ConteudoContexto/ItemCarreira.cs
+++ ProjetoBaltaPOO/ConteudoContexto/ItemCarreira.cs
@@ -2,9 +2,17 @@
 {
     public class ItemCarreira
     {
-        public int Ordem { get; set; }
+        public int Pedido { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public Curso Curso { get; set; }
+
+        public ItemCarreira(int pedido, string titulo, string descricao, Curso curso)
+        {
+            Pedido = pedido;
+            Titulo = titulo;
+            Descricao = descricao;
+            Curso = curso;
+        }
     }
 }
--- ProjetoBaltaPOO/Program.cs
+++ ProjetoBaltaPOO/Program.cs
@@ -6,20 +6,50 @@ namespace ProjetoBaltaPOO
     {
         static void Main(string[] args)
         {
-            var artigo = new List<Artigo>();
+            //var artigo = new List<Artigo>();
 
-            artigo.Add(new Artigo("Artigo sobre OOP", "orientacao-objetos"));
-            artigo.Add(new Artigo("Artigo sobre C#", "c-sharp"));
-            artigo.Add(new Artigo("Artigo sobre .NET", "dotnet"));
+            //artigo.Add(new Artigo("Artigo sobre OOP", "orientacao-objetos"));
+            //artigo.Add(new Artigo("Artigo sobre C#", "c-sharp"));
+            //artigo.Add(new Artigo("Artigo sobre .NET", "dotnet"));
 
-            foreach (var item in artigo)
+            //foreach (var item in artigo)
+            //{
+            //    Console.WriteLine("-------------------");
+            //    Console.WriteLine(item.Id);
+            //    Console.WriteLine(item.Titulo);
+            //    Console.WriteLine(item.Url);
+            //}
+
+            //Console.WriteLine("-------------------");
+
+            var cursos = new List<Curso>();
+            var cursoOOP = new Curso("Fundamentos OOP", "fundamentos-oop");
+            var cursoCsharp = new Curso("Fundamentos C#", "fundamentos-csharp");
+            var cursoAspNet = new Curso("Fundamentos ASP.NET", "fundamentos-aspnet");
+
+            cursos.Add(cursoOOP);
+            cursos.Add(cursoCsharp);
+            cursos.Add(cursoAspNet);
+
+            var carreiras = new List<Carreira>();
+            var carreiraDotNet = new Carreira("Especialista .NET", "especialista-dotnet");
+            var carreiraItem3 = new ItemCarreira(3, "Termine a carreira aqui", "", null);
+            var carreiraItem1 = new ItemCarreira(1, "Comece a carreira por aqui", "", null);
+            var carreiraItem2 = new ItemCarreira(2, "Continue a carreira por aqui", "", null);
+            carreiraDotNet.Itens.Add(carreiraItem3);
+            carreiraDotNet.Itens.Add(carreiraItem1);
+            carreiraDotNet.Itens.Add(carreiraItem2);
+            carreiras.Add(carreiraDotNet);
+
+            foreach (var carreira in carreiras)
             {
-                Console.WriteLine("-------------------");
-                Console.WriteLine(item.Id);
-                Console.WriteLine(item.Titulo);
-                Console.WriteLine(item.Url);
-            }
-            Console.WriteLine("-------------------");
+                Console.WriteLine(carreira.Titulo);
+                Console.WriteLine(carreira.Url);
+                foreach (var item in carreira.Itens.OrderBy(x => x.Pedido))
+                {
+                    Console.WriteLine($"{item.Pedido} - {item.Titulo}");
+                }
+            }         
 
             Console.ReadKey();
         }
